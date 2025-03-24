2a0cda3eb5bbd823aa27a6082fbd9e38b0a340e4
Cypher <doccamarco@gmail.com>
2025-03-23 22:32:13 -0400

    Add SkeletonUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script>
+	let theme = $state('light');
+	function toggleTheme() {
+		let outerHTML = document.getElementsByTagName('html')[0];
+		if (theme === 'light') {
+			theme = 'dark';
+			outerHTML.className = 'dark';
+		} else {
+			theme = 'light';
+			outerHTML.className = 'light';
+		}
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a>
+<button onclick={() => toggleTheme()} class="btn preset-filled-primary-500">
+	Dark/Light Mode
+</button>
