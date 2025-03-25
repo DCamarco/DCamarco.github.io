53797df903ebe17cf9c8aeb7c6533a25a94a134e
Cypher <doccamarco@gmail.com>
2025-03-24 20:53:33 -0400

    Added header skeleton

---
+++ src/lib/components/Navigation/Navigation.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+
+	let value = $state('');
+</script>
+
+<Navigation.Bar
+	{value}
+	onValueChange={(newValue) => (value = newValue)}
+	background="bg-transparent"
+	tilesJustify="justify-between"
+	padding="px-40"
+	classes="sticky top-0 z-50"
+>
+	<div class="flex items-center justify-center">
+		<Navigation.Tile href="#/"><h1>Dominic.</h1></Navigation.Tile>
+	</div>
+	<div class="flex items-center justify-evenly">
+		<Navigation.Tile href="#about">About</Navigation.Tile>
+		<Navigation.Tile href="#projects">Projects</Navigation.Tile>
+		<Navigation.Tile href="#contact">Contact</Navigation.Tile>
+	</div>
+</Navigation.Bar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import Navigation from '../lib/components/Navigation/Navigation.svelte';
 
 	let { children } = $props();
 </script>
 
 <div class="container">
+	<Navigation />
+	<span class="my-96"></span>
+	<span class="my-96"></span>
 	{@render children()}
 </div>
