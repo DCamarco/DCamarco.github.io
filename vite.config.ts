5010ac50987eedbb5deb97c9569fdc75d74809fb
Cypher <doccamarco@gmail.com>
2025-03-30 17:56:44 -0400

    Optimized Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), nodeResolve({ preferBuiltins: true })],
+	optimizeDeps: {
+		exclude: ['rollup']
+	}
 });
